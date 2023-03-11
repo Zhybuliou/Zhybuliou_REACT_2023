3f2c643683751ee9c879c4554f95b1ac1b88c8d2
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-03-11 23:03:13 +0300

    feat: start task react-components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PureComponent } from 'react';
+import './App.scss';
+
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <>
+        <h1>Start page</h1>
+      </>
+    );
+  }
+}
