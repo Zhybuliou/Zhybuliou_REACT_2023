1b26cb4160098c9aeb70006466a2635708140db4
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-03-20 22:47:40 +0300

    feat: add data inputs

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -18,6 +18,7 @@ export default class FormPage extends Component<MyProps> {
       <>
         <h2>Form</h2>
         <form id="form" className="form" name="control-ref" ref={this.formRadio} />
+        <input className="form-button" type="submit" value="Send" />
       </>
     );
   }
