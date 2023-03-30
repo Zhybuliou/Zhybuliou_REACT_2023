9c8b0d8bcabc59548635015aedd73cd1b4ae0a1b
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-03-30 11:58:35 +0300

    init: start task react-hooks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
-import { PureComponent } from 'react';
 import Cards from '../components/Cards';
 import SearchBox from '../components/SearchBox';
 
-export default class Home extends PureComponent {
-  render() {
-    return (
-      <>
-        <h2>Home</h2>
-        <SearchBox />
-        <Cards />
-      </>
-    );
-  }
+export default function Home() {
+  return (
+    <>
+      <h2>Home</h2>
+      <SearchBox />
+      <Cards />
+    </>
+  );
 }
